d4917373547893819a2c2ed37c1cc185920e2338
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-03 15:12:25 -0400

    Finished first tutorial image

---
+++ Todolist/Views/ListView.swift
@@ -0,0 +1,50 @@
+//
+//  ListView.swift
+//  Todolist
+//
+//  Created by Cooper Lawson on 2023-04-03.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    // MARK Computed Properties
+    var body: some View {
+
+        NavigationView {
+            
+            VStack {
+                
+                HStack {
+                    
+                    TextField("Enter a to-do item", text: Binding.constant(""))
+                    
+                    Button(action: {
+                    }, label: {
+                        Text("ADD")
+                            .font(.caption)
+                    })
+                }
+                .padding(20)
+                
+                List {
+                    
+                    }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
