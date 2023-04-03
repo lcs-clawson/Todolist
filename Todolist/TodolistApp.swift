adef9b8af846706acc4e22fb34207c0d9c127962
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-03 15:00:06 -0400

    Initial Commit

---
+++ Todolist/TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by Cooper Lawson on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
