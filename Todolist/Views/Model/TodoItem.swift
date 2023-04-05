660766ee3d052573bf1615609254db352147db16
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-05 09:43:02 -0400

    Now reading information from the blackbird database

--- Todolist/Views/Model/TodoItem.swift
+++ Todolist/Views/Model/TodoItem.swift
@@ -5,26 +5,11 @@
 //  Created by Cooper Lawson on 2023-04-03.
 //
 
+import Blackbird
 import Foundation
 
-struct Todoitem: Identifiable {
-    var id: Int
-    var description: String
-    var completed: Bool
+struct Todoitem: BlackbirdModel {
+    @BlackbirdColumn var id: Int
+    @BlackbirdColumn var description: String
+    @BlackbirdColumn var completed: Bool
 }
-
-var existingToDoItems = [
-Todoitem(id: 1, description: "Study for phisycs quiz", completed: false)
-
-,
-
-Todoitem(id: 2, description: "Finish computer science assignment", completed: true)
-
-,
-
-
-Todoitem(id: 3, description: "Go for a run", completed: false)
-
-,
-
-]
--- Todolist/Views/Model/TodolistApp.swift
+++ Todolist/Views/Model/TodolistApp.swift
@@ -5,6 +5,7 @@
 //  Created by Cooper Lawson on 2023-04-03.
 //
 
+import Blackbird
 import SwiftUI
 
 @main
@@ -12,6 +13,8 @@ struct TodolistApp: App {
     var body: some Scene {
         WindowGroup {
             ListView()
+         //Making the database available to other views.
+                .environment(\.blackbirdDatabase, AppDatabase.instance)
         }
     }
 }
